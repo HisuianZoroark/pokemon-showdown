64aadfdadf8fad4d414b9e5edcaa9aa08b3a292a
We Wuz Nidokangz <globalistgamer@gmail.com>
2021-07-28 21:47:04 +0900

    Fix 350 Cup rules

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -831,7 +831,7 @@ export const Formats: FormatList = [
 		name: "[Gen 8] 350 Cup",
 		desc: "Pok&eacute;mon with a base stat total of 350 or lower get their stats doubled. &bullet; <a href=\"https://www.smogon.com/forums/threads/350-cup.3656554/\">350 Cup</a>",
 		mod: 'gen8',
-		ruleset: ['[Gen 8] Ubers', '350 Cup Rule'],
+		ruleset: ['Standard', 'Dynamax Clause', '350 Cup Rule'],
 		banlist: ['Abra', 'Gastly', 'Pawniard', 'Rufflet', 'Woobat', 'Eviolite', 'Light Ball', 'Arena Trap', 'Shadow Tag', 'Baton Pass'],
 	},
 	{
