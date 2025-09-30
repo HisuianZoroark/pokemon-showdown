bd0d1f1d3d065e84998377493277516ac4f0384c
HiZo <96159984+HisuianZoroark@users.noreply.github.com>
2025-09-29 20:35:17 -0400

    zzz

--- data/mods/biomechmons/scripts.ts
+++ data/mods/biomechmons/scripts.ts
@@ -5,6 +5,8 @@
 //
 // I regret this now
 
+import { RESTORATIVE_BERRIES } from '../../../sim/pokemon';
+
 
 export const Scripts: ModdedBattleScriptsData = {
   pokemon: {
@@ -19,18 +21,18 @@ export const Scripts: ModdedBattleScriptsData = {
           effectType: "Item",
           flags: {},
           toString() {
-  					return move.name;
-  				},
+            return move.name;
+          },
           onBioMechMons(this: Battle, pokemon: Pokemon) {
             const sketchedMove = {
-      				move: move.name,
-      				id: move.id,
-      				pp: move.pp,
-      				maxpp: move.pp,
-      				target: move.target,
-      				disabled: false,
-      				used: false,
-      			};
+              move: move.name,
+              id: move.id,
+              pp: move.noPPBoosts ? move.pp : move.pp * 8 / 5,
+              maxpp: move.noPPBoosts ? move.pp : move.pp * 8 / 5,
+              target: move.target,
+              disabled: false,
+              used: false,
+            };
             pokemon.moveSlots.push(sketchedMove);
           },
         } as Item;
@@ -46,11 +48,11 @@ export const Scripts: ModdedBattleScriptsData = {
           effectType: "Item",
           toString() {
             return ability.name;
-  				},
+          },
         } as Item;
         for (const prop of Object.keys(ability)) {
           // @ts-ignore
-          if (typeof ability[prop] ==='function' || prop.toLowerCase().endsWith('priority')) {
+          if (typeof ability[prop] === 'function' || prop.toLowerCase().endsWith('priority')) {
             // @ts-ignore
             abilItem[prop] = ability[prop];
           }
@@ -70,18 +72,18 @@ export const Scripts: ModdedBattleScriptsData = {
           fullname: 'ability: ' + move.name,
           effectType: "Ability",
           toString() {
-  					return move.name;
-  				},
+            return move.name;
+          },
           onBioMechMons(this: Battle, pokemon: Pokemon) {
             const sketchedMove = {
-      				move: move.name,
-      				id: move.id,
-      				pp: move.pp,
-      				maxpp: move.pp,
-      				target: move.target,
-      				disabled: false,
-      				used: false,
-      			};
+              move: move.name,
+              id: move.id,
+              pp: move.noPPBoosts ? move.pp : move.pp * 8 / 5,
+              maxpp: move.noPPBoosts ? move.pp : move.pp * 8 / 5,
+              target: move.target,
+              disabled: false,
+              used: false,
+            };
             pokemon.moveSlots.push(sketchedMove);
           },
         } as Ability;
@@ -96,18 +98,125 @@ export const Scripts: ModdedBattleScriptsData = {
           flags: {},
           toString() {
             return item.name;
-  				},
+          },
         } as Ability;
         for (const prop of Object.keys(item)) {
           // @ts-ignore
-          if (typeof item[prop] ==='function' || prop.toLowerCase().endsWith('priority')) {
+          if (typeof item[prop] === 'function' || prop.toLowerCase().endsWith('priority')) {
             // @ts-ignore
             abilItem[prop] = item[prop];
           }
         }
         return abilItem;
       }
-  		return this.battle.dex.abilities.getByID(this.ability);
+      return this.battle.dex.abilities.getByID(this.ability);
     },
-  }
+    // I dont want to hard code every consumable item
+    eatItem(force, source, sourceEffect) {
+      if (!this.item || this.itemState.knockedOff) return false;
+      if ((!this.hp && this.item !== 'jabocaberry' && this.item !== 'rowapberry') || !this.isActive) return false;
+
+      if (!sourceEffect && this.battle.effect) sourceEffect = this.battle.effect;
+      if (!source && this.battle.event?.target) source = this.battle.event.target;
+      const item = this.getItem();
+      if (sourceEffect?.effectType === 'Item' && this.item !== sourceEffect.id && source === this) {
+        // if an item is telling us to eat it but we aren't holding it, we probably shouldn't eat what we are holding
+        return false;
+      }
+      if (
+        this.battle.runEvent('UseItem', this, null, null, item) &&
+        (force || this.battle.runEvent('TryEatItem', this, null, null, item))
+      ) {
+        this.battle.add('-enditem', this, item, '[eat]');
+
+        this.battle.singleEvent('Eat', item, this.itemState, this, source, sourceEffect);
+        this.battle.runEvent('EatItem', this, source, sourceEffect, item);
+
+        if (RESTORATIVE_BERRIES.has(item.id)) {
+          switch (this.pendingStaleness) {
+            case 'internal':
+              if (this.staleness !== 'external') this.staleness = 'internal';
+              break;
+            case 'external':
+              this.staleness = 'external';
+              break;
+          }
+          this.pendingStaleness = undefined;
+        }
+
+        switch (sourceEffect?.effectType) {
+          case 'Ability':
+            this.setAbility('No Ability', this);
+            this.baseAbility = this.ability;
+            break;
+          case 'Move': // todo: Find out
+            if (source === this) {
+              break;
+            }
+          // fall through otherwise
+          default:
+            this.lastItem = this.item;
+            this.item = '';
+            this.battle.clearEffectState(this.itemState);
+        }
+        this.usedItemThisTurn = true;
+        this.ateBerry = true;
+        this.battle.runEvent('AfterUseItem', this, null, null, item);
+        return true;
+      }
+      return false;
+    },
+
+    useItem(source, sourceEffect) {
+      if ((!this.hp && !this.getItem().isGem) || !this.isActive) return false;
+      if (!this.item || this.itemState.knockedOff) return false;
+
+      if (!sourceEffect && this.battle.effect) sourceEffect = this.battle.effect;
+      if (!source && this.battle.event?.target) source = this.battle.event.target;
+      const item = this.getItem();
+      if (sourceEffect?.effectType === 'Item' && this.item !== sourceEffect.id && source === this) {
+        // if an item is telling us to eat it but we aren't holding it, we probably shouldn't eat what we are holding
+        return false;
+      }
+      if (this.battle.runEvent('UseItem', this, null, null, item)) {
+        switch (item.id) {
+          case 'redcard':
+            this.battle.add('-enditem', this, item, `[of] ${source}`);
+            break;
+          default:
+            if (item.isGem) {
+              this.battle.add('-enditem', this, item, '[from] gem');
+            } else {
+              this.battle.add('-enditem', this, item);
+            }
+            break;
+        }
+        if (item.boosts) {
+          this.battle.boost(item.boosts, this, source, item);
+        }
+
+        this.battle.singleEvent('Use', item, this.itemState, this, source, sourceEffect);
+
+        switch (sourceEffect?.effectType) {
+          case 'Ability':
+            this.setAbility('No Ability', this);
+            this.baseAbility = this.ability;
+            break;
+          case 'Move': // todo: Find out
+            if (source === this) {
+              break;
+            }
+          // fall through otherwise
+          default:
+            this.lastItem = this.item;
+            this.item = '';
+            this.battle.clearEffectState(this.itemState);
+        }
+        this.usedItemThisTurn = true;
+        this.battle.runEvent('AfterUseItem', this, null, null, item);
+        return true;
+      }
+      return false;
+    },
+  },
 };
